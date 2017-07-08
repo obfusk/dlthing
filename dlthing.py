83fadb68f9f2bd857cf8971b3d26259ca4ecf0d6
Felix C. Stegerman <flx@obfusk.net>
2017-07-08 15:47:31 +0200

    also add play_path to url

--- dlthing.py
+++ dlthing.py
@@ -24,13 +24,14 @@ def index():
     **(get_video_url(request.form["url"])
       if request.method == "POST" else {}))
 
-# TODO: ensure url? what if no url?
 def get_video_url(url):
   message, vid_url = None, None; opts = dict(format = "best")
   with youtube_dl.YoutubeDL(opts) as ydl:
     try:
-      vid_url = ydl.extract_info(url, download = False).get("url")
-      if not vid_url: message = "youtube-dl did not return a URL"
+      info_dict = ydl.extract_info(url, download = False)
+      vid_url   = info_dict.get("url")
+      if vid_url: vid_url += info_dict.get("play_path", "")
+      else: message = "youtube-dl did not return a URL"
     except youtube_dl.utils.DownloadError as e:
       message = "not a valid URL" if "not a valid URL" in e.args[0] \
                 else "download error"
